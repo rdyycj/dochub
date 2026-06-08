a477f88fc06650d7b4d2b8da3853ae8d7dd69eee
rdyycj <2685095359@qq.com>
2026-06-09 03:36:41 +0800

    chore: scaffold Electron + React + TypeScript project

---
+++ src/main/index.ts
@@ -0,0 +1,32 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+let mainWindow: BrowserWindow | null = null;
+
+function createWindow(): void {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.loadURL('http://localhost:5173');
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
---
+++ src/renderer/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">DocHub</h1>
+    </div>
+  );
+};
+
+export default App;
